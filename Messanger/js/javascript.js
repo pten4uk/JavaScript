ed3f69b1f774ffa34e22571ed27e7fdf1a18002f
pten4uk <pten4ik99@yandex.ru>
2021-11-15 23:04:03 +0300

    changes

--- Messanger/js/javascript.js
+++ Messanger/js/javascript.js
@@ -1,13 +1,2 @@
 `use strict`;
 
-let catalogElem = document.querySelector(`.list-item.dropdown`);
-
-let dropDown = catalogElem.querySelector(`.nav-dropdown`);
-
-catalogElem.addEventListener(`mouseenter`, () => {
-    dropDown.classList.add(`active`)
-})
-
-catalogElem.addEventListener(`mouseleave`, () => {
-    dropDown.classList.remove(`active`)
-})
