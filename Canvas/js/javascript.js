5e8deee831fa8605bd928e03dacc6be30332d304
pten4uk <pten4ik99@yandex.ru>
2021-12-01 00:26:23 +0300

    ваыавы

--- Canvas/js/javascript.js
+++ Canvas/js/javascript.js
@@ -1,48 +1,92 @@
 "use strict";
 
-const props = {
-    spaceDiametr: 80
-}
-
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 
 let w = canvas.width = innerWidth;
 let h = canvas.height = innerHeight;
-
-let dotsList;
-canvas.style.background = 'rgba(17,17,23,1)';
+let particleArray = [];
 
 document.body.append(canvas);
+canvas.style.background = 'radial-gradient(blue, black)'
 
-class Dot {
+const mouse = {
+    x: null,
+    y: null,
+    radius: 150
+}
+
+canvas.addEventListener('mousemove', event => {
+    mouse.x = event.x;
+    mouse.y = event.y;
+})
+
+ctx.fillStyle = 'white'
+ctx.font = '90px Verdana';
+ctx.fillText('A', 0, 40);
+
+const textCoords = ctx.getImageData(0,0,100,100);
+
+class Particle {
     constructor(x, y) {
         this.x = x;
-        this.y = x;
+        this.y = y;
+        this.size = 3;
+        this.baseX = this.x;
+        this.baseY = this.y;
+        this.dencity = Math.random() * 30 + 1;
     }
     draw() {
+        ctx.fillStyle = 'white';
         ctx.beginPath();
-        ctx.arc(this.x, this.y, 40, 0, 2 * Math.PI, false);
+        ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
         ctx.closePath();
-        ctx.fillStyle = 'red';
         ctx.fill();
     }
-}
+    update() {
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;
+        let distance = (dx**2 + dy**2)**0.5;
 
-init();
-function init() {
-    dotsList = [];
+        let fDirectionX = dx / distance;
+        let fDirectionY = dy / distance;
+        let maxDistance = mouse.radius;
+        let force = (maxDistance - distance) / maxDistance;
+        let directionX = fDirectionX * force * this.dencity;
+        let directionY = fDirectionY * force * this.dencity;
 
-    const dotsCountX = w / props.spaceDiametr | 0;
-    const dotsCountY = h / props.spaceDiametr | 0;
-    const startX = props.spaceDiametr / 2;
-    const startY = props.spaceDiametr / 2;
-
-
-    let y = startY;
-
-    for (let i; i < dotsCountX; i++) {
-        let x = startX + i * props.spaceDiametr;
-        dotsList.push(new Dot(x, y))
+        if (distance < mouse.radius) {
+            this.x -= directionX;
+            this.y -= directionY;
+        } else {
+            if (this.x != this.baseX) {
+                let dx = this.x - this.baseX;
+                this.x -= dx/60;
+            }
+            if (this.y != this.baseY) {
+                let dy = this.y - this.baseY;
+                this.y -= dy/60;
+            }
+        }
     }
 }
+
+function init() {
+    particleArray = [];
+    for (let i = 0; i < 1000; i++) {
+        let x = Math.random() * w;
+        let y = Math.random() * h;
+        particleArray.push(new Particle(x, y))
+    }
+}
+init();
+
+function animate() {
+    ctx.clearRect(0, 0, w, h);
+    particleArray.forEach(elem => {
+        elem.draw();
+        elem.update();
+    });
+    requestAnimationFrame(animate);
+}
+animate();
