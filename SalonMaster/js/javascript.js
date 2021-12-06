69ec910a6d569eda3ecf0bb7430513cc5abdb2d6
pten4uk <pten4ik99@yandex.ru>
2021-12-06 19:43:03 +0300

    SalonMaster

--- SalonMaster/js/javascript.js
+++ SalonMaster/js/javascript.js
@@ -1,1 +1,27 @@
 "use strict";
+
+let blockProduct = document.querySelector(`article`);
+let main = document.querySelector(`main`);
+
+for (let i = 0; i < 10; i++) {
+    let article = document.createElement(`article`);
+    article.innerHTML = blockProduct.innerHTML;
+    main.append(article);
+}
+
+window.onload = () => {
+    document.body.style.opacity = `100%`
+}
+
+document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'ArrowDown':
+            event.preventDefault();
+            console.log('DOWN')
+            break
+        case 'ArrowUp':
+            event.preventDefault();
+            console.log('UP')
+            break
+    }
+})
