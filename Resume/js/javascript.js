499e221f17a3997ab6fd58f8952d335b4499f38e
pten4uk <pten4ik99@yandex.ru>
2021-12-23 20:25:41 +0300

    Start Resume

--- Resume/js/javascript.js
+++ Resume/js/javascript.js
@@ -1,1 +1,40 @@
 `use strict`;
+
+let baseSection = document.querySelector('section.base');
+let skillsSection = document.querySelector('section.skills');
+let specializationSection = document.querySelector('section.specialization');
+
+let skillsNav = document.querySelector('nav.skills');
+let infoNav = document.querySelector('nav.info');
+let specNav = document.querySelector('nav.spec');
+
+
+skillsNav.addEventListener('click', () => {
+    skillsNav.classList.add('selected');
+    infoNav.classList.remove('selected');
+    specNav.classList.remove('selected');
+
+    baseSection.classList.remove('active');
+    skillsSection.classList.add('active');
+    specializationSection.classList.remove('active');
+})
+
+infoNav.addEventListener('click', () => {
+    skillsNav.classList.remove('selected');
+    infoNav.classList.add('selected');
+    specNav.classList.remove('selected');
+
+    baseSection.classList.add('active');
+    skillsSection.classList.remove('active');
+    specializationSection.classList.remove('active');
+})
+
+specNav.addEventListener('click', () => {
+    skillsNav.classList.remove('selected');
+    infoNav.classList.remove('selected');
+    specNav.classList.add('selected');
+
+    baseSection.classList.remove('active');
+    skillsSection.classList.remove('active');
+    specializationSection.classList.add('active');
+})
